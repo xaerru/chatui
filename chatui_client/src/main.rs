919255ab87e5f5614ac64d5675227f010ed33ba5
grvxs <gauravsinghsisodia5@gmail.com>
2021-06-26 00:32:11 +0530

    New functions for readable code

--- chatui_client/src/main.rs
+++ chatui_client/src/main.rs
@@ -1,6 +1,6 @@
 use std::io::{self, ErrorKind, Read, Write};
 use std::net::TcpStream;
-use std::sync::mpsc::{self, TryRecvError};
+use std::sync::mpsc::{self, Sender, TryRecvError};
 use std::time::Duration;
 use std::{process, thread};
 
@@ -19,6 +19,33 @@ fn get_name() -> String {
     name.trim().to_string()
 }
 
+fn send(tx: Sender<String>) {
+    loop {
+        let mut buff = String::new();
+        io::stdin()
+            .read_line(&mut buff)
+            .expect("Reading from stdin failed.");
+        if tx.send(buff.trim().to_string()).is_err() {
+            break;
+        }
+    }
+}
+
+fn parse(buff: Vec<u8>) {
+    let msg = String::from_utf8(
+        buff.into_iter()
+            .take_while(|&x| x != 0)
+            .collect::<Vec<u8>>(),
+    )
+    .expect("Invalid utf8 message.");
+    let data: Value = serde_json::from_str(&msg).expect("Failed to parse data.");
+    println!(
+        "{}: {}",
+        data["name"].as_str().unwrap(),
+        data["message"].as_str().unwrap()
+    );
+}
+
 fn main() {
     let name = get_name();
 
@@ -35,18 +62,7 @@ fn main() {
 
         match client.read_exact(&mut buff) {
             Ok(_) => {
-                let msg = String::from_utf8(
-                    buff.into_iter()
-                        .take_while(|&x| x != 0)
-                        .collect::<Vec<u8>>(),
-                )
-                .expect("Invalid utf8 message.");
-                let data: Value = serde_json::from_str(&msg).expect("Failed to parse data.");
-                println!(
-                    "{}: {}",
-                    data["name"].as_str().unwrap(),
-                    data["message"].as_str().unwrap()
-                );
+                parse(buff);
             }
             Err(ref err) if err.kind() == ErrorKind::WouldBlock => {}
             Err(_) => {
@@ -70,14 +86,7 @@ fn main() {
         thread::sleep(Duration::from_millis(100));
     });
 
-    loop {
-        let mut buff = String::new();
-        io::stdin()
-            .read_line(&mut buff)
-            .expect("Reading from stdin failed.");
-        if tx.send(buff.trim().to_string()).is_err() {
-            break;
-        }
-    }
+    send(tx);
+
     println!("Bye.");
 }
